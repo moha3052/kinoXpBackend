cec5224a75af4392c182ed7574cc163c01b64abb
Lenovo <joda0002@st>
2024-10-03 16:25:03 +0200

    Crud metoder for movie oprettet

--- src/main/java/repository/MovieRepository.java
+++ src/main/java/repository/MovieRepository.java
@@ -1,4 +1,12 @@
 package repository;
 
-public class MovieRepository {
+import model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    List<Movie> findByid(int id);
 }
--- src/main/java/service/MovieService.java
+++ src/main/java/service/MovieService.java
@@ -1,4 +1,48 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import model.Movie;
+import repository.MovieRepository;
+
+import java.util.List;
+
+@Service
 public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    // Hent alle film
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    // Tilføj en ny film
+    public Movie addMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    // Hent en film baseret på ID
+    public List<Movie> getMovieById(int id) {
+        return movieRepository.findByid(id);
+    }
+
+    // Opdater en film
+    public Movie updateMovie(int id, Movie movieDetails) {
+        Movie movie = movieRepository.findById(id).orElse(null);
+        if (movie != null) {
+            movie.setTitle(movieDetails.getTitle());
+            movie.setGenre(movieDetails.getGenre());
+            movie.setDuration(movieDetails.getDuration());
+            movie.setAgeLimit(movieDetails.getAgeLimit());
+            return movieRepository.save(movie);
+        }
+        return null;
+    }
+
+    // Slet en film
+    public void deleteMovie(int id) {
+        movieRepository.deleteById(id);
+    }
 }
