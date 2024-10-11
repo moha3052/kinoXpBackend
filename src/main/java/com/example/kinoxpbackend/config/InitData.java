99a7eb3679b275d7399da2e9099174da294fdc91
moha3052 <moha3052@hotmail.dk>
2024-10-11 12:42:42 +0200

    Ny

---
+++ src/main/java/com/example/kinoxpbackend/config/InitData.java
@@ -0,0 +1,10 @@
+package com.example.kinoxpbackend.config;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class InitData implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        // if db is not populated, then add movies
+    }
+}
